3da11af503693a975173511caa5353b4b8a9cbeb
zt23340 <zt23340@ztn.cn>
2026-06-28 01:38:36 +0800

    feat: 新增测试

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button @click="handleClick">Click me</button>
   </div>
 </template>
+
+<script setup lang="ts">
+const handleClick = () => {
+  alert("Button clicked");
+};
+</script>
 
 <style>
 @media (min-width: 1024px) {
